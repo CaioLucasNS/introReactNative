c036d390ff5fbccbf1f4969b1025462bc0d3cfa9
DESKTOP-PKAI836\Caio <lcaio1281@gmail.com>
2022-04-07 01:38:30 -0300

    some adjusts in EmployeeData component

--- src/components/EmployeeData/EmployeeData.js
+++ src/components/EmployeeData/EmployeeData.js
@@ -6,11 +6,11 @@ const EmployeeData = props => {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.text}>Nome: {props.name || '-'}</Text>
+        <Text style={styles.text}>Nome: {props.name}</Text>
 
-        <Text style={styles.text}>Idade: {props.age || '-'}</Text>
+        <Text style={styles.text}>Idade: {props.age}</Text>
 
-        <Text style={styles.text}>Stack: {props.stack || '-'}</Text>
+        <Text style={styles.text}>Stack: {props.stack}</Text>
       </View>
     </View>
   );
